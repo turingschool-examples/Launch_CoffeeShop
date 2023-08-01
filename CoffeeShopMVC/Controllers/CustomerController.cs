4421f4903252a055f3f30ab1a5e7cba61e430c28
Eli-J-Paris <Eli.J.Paris@gmail.com>
2023-08-01 10:33:27 -0600

    Added Controller

---
+++ CoffeeShopMVC/Controllers/CustomerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShopMVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

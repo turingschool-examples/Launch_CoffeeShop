107a41586f4de3d73dde30727e276da736f2e0f9
skylarbsandler <skylarbsandler@gmail.com>
2023-08-01 15:10:34 -0600

    Added Customers New and Index

---
+++ CoffeeShopMVC/Controllers/CustomersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CoffeeShopMVC.DataAccess;
+using CoffeeShopMVC.Models;
+using CoffeeShopMVC.Model;
+
+namespace CoffeeShopMVC.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly CoffeeShopMVCContext _context;
+
+        public CustomersController(CoffeeShopMVCContext Context)
+        {
+            _context = Context;
+        }
+
+        public IActionResult Index()
+        {
+            var customers = _context.Customers;
+
+            return View(customers);
+        }
+    }
+}

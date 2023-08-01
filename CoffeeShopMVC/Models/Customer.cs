b515c2ccf44998d64e54ca849657cef96510997b
Eli-J-Paris <Eli.J.Paris@gmail.com>
2023-08-01 12:00:35 -0600

    Add- show page for a customer

--- CoffeeShopMVC/Models/Customer.cs
+++ CoffeeShopMVC/Models/Customer.cs
@@ -6,5 +6,32 @@
         public string Name { get; set; }
         public string Email { get; set; }
         public List<Order> Orders { get; set; }
+
+
+        public string TotalSpent()
+        {
+            int sum = 0;
+
+            foreach(var order in Orders)
+            {
+                foreach(var item in order.Items)
+                {
+                    sum += item.PriceInCents;
+                }
+            }
+            return $"${Math.Round(Convert.ToDouble(sum) / 100,2)}";
+        }
+
+        public List<string> AllItems()
+        {
+            var allItems = new List<Item>();
+
+            foreach(var order in Orders)
+            {
+                allItems.AddRange(order.Items);
+            }
+
+            return allItems.Select(i => i.Name).ToList();
+        }
     }
 }

ffacc9a9029b3d9fe1320bad3ec7860d3c74e138
RafiWick <rafiwick@gmail.com>
2023-08-01 15:36:28 -0600

    order ready to start crud

---
+++ CoffeeShopMVC/Controllers/OrdersController.cs
@@ -0,0 +1,20 @@
+﻿using CoffeeShopMVC.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShopMVC.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly CoffeeShopMVCContext _context;
+
+        public OrdersController(CoffeeShopMVCContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

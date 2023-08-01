a67b43a7683ce6a09121ad4a1bd7905af59feb61
RafiWick <rafiwick@gmail.com>
2023-08-01 10:21:10 -0600

    order and customer models created and added to Db

--- CoffeeShopMVC/DataAccess/CoffeeShopMVCContext.cs
+++ CoffeeShopMVC/DataAccess/CoffeeShopMVCContext.cs
@@ -6,6 +6,8 @@ namespace CoffeeShopMVC.DataAccess
     public class CoffeeShopMVCContext : DbContext 
     {
         public DbSet<Item> Items { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
 
         public CoffeeShopMVCContext(DbContextOptions<CoffeeShopMVCContext> options) : base(options)
         {

070049ec75d033cfb765578058973d2d11a712d0
Eli-J-Paris <Eli.J.Paris@gmail.com>
2023-08-01 13:47:56 -0600

    FixedItemModel

--- CoffeeShopMVC/Models/Item.cs
+++ CoffeeShopMVC/Models/Item.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public int PriceInCents { get; set; }
-        public Order Order { get; set; }
+        public List<Order> Orders { get; set; } = new List<Order>();
 
 
         public string PriceInDollars()

8e14c0afb46f2c58566a5dccc0affb497c3d44b2
skylarbsandler <skylarbsandler@gmail.com>
2023-08-02 10:51:29 -0600

    Added Customer Controller Tests

---
+++ CoffeeShopMVC/Migrations/20230802161708_RebuildDb.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CoffeeShopMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class RebuildDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "customer",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    email_address = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_customer", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "order",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    customer_id = table.Column<int>(type: "integer", nullable: false),
+                    date_created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_order", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_order_customer_customer_id",
+                        column: x => x.customer_id,
+                        principalTable: "customer",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "items",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    price_in_cents = table.Column<int>(type: "integer", nullable: false),
+                    order_id = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_items", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_items_order_order_id",
+                        column: x => x.order_id,
+                        principalTable: "order",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_items_order_id",
+                table: "items",
+                column: "order_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_order_customer_id",
+                table: "order",
+                column: "customer_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "items");
+
+            migrationBuilder.DropTable(
+                name: "order");
+
+            migrationBuilder.DropTable(
+                name: "customer");
+        }
+    }
+}
---
+++ CoffeeShopTests/CustomerControllerTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CoffeeShopMVC.FeatureTests;
+using CoffeeShopMVC.DataAccess;
+using CoffeeShopMVC.Models;
+
+namespace CoffeeShopTests.FeatureTests
+{
+    [Collection("Controller Tests")]
+    public class CustomerControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public CustomerControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        private CoffeeShopMVCContext GetDbContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<CoffeeShopMVCContext>();
+            optionsBuilder.UseInMemoryDatabase("TestDatabase");
+
+            var context = new CoffeeShopMVCContext(optionsBuilder.Options);
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            return context;
+        }
+
+        [Fact]
+        public async Task Test_Index_ReturnsViewWithCustomers()
+        {
+            var context = GetDbContext();
+            context.Customers.Add(new Customer { Name = "Jeff", EmailAddress = "jeff@mail.com" });
+            context.Customers.Add(new Customer { Name = "Yeff", EmailAddress = "yeff@mail.com" });
+            context.SaveChanges();
+
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync("/Customers");
+            var html = await response.Content.ReadAsStringAsync();
+
+            response.EnsureSuccessStatusCode();
+            Assert.Contains("Jeff", html);
+            Assert.Contains("Yeff", html);
+            Assert.Contains("yeff@mail.com", html);
+            Assert.Contains("jeff@mail.com", html);
+        }
+    }
+}
